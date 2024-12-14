edc71083313ccc3dbf904d2eb135013746f61705
Oleksandr <oleksandr.nediev@student.howest.be>
2024-12-14 19:34:58 +0100

    Add 2 workflows

--- ClassLib/StudentRepository.cs
+++ ClassLib/StudentRepository.cs
@@ -12,7 +12,8 @@ namespace ClassLib
             {
                 new Student { Id = 1, Name = "Mieke", Succeeded = true },
                 new Student { Id = 2, Name = "Mike" },
-                new Student { Id = 3, Name = "Kevin", Succeeded = false}
+                new Student { Id = 3, Name = "Kevin", Succeeded = false},
+                new Student { Id = 4, Name = "Staging", Succeeded = false},
             };
         }
 
